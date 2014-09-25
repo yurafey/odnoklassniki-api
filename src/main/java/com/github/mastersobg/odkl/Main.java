ece90a487ed28318115a6814ff55ad05e0547828
yurafey <yurafey@gmail.com>
2014-09-25 22:07:50 +0400

    test main

---
+++ src/main/java/com/github/mastersobg/odkl/Main.java
@@ -0,0 +1,17 @@
+package com.github.mastersobg.odkl;
+
+import com.github.mastersobg.odkl.auth.ApiConfig;
+
+/**
+ * Created by yuraf_000 on 25.09.2014.
+ */
+public class Main {
+
+    public static void main(String[] args){
+        OdklApi api = new OdklApi(ApiConfig.APP_ID, ApiConfig.APP_PUBLIC_KEY, ApiConfig.APP_SECRET_KEY,ApiConfig.ACCESS_TOKEN , ApiConfig.REFRESH_TOKEN);
+        PhotoGrabber gr = new PhotoGrabber(api);
+        //574881296141 - Martin (2 friends)
+        //559653274945 - Rudolf (4 friends)
+        gr.recursiveGrab("574881296141", 2);
+    }
+}
